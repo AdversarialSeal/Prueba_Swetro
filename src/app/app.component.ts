05c80c96c6de8493c1722c12e6032793588f1853
Kevin Edilson Valencia Romero <69984015+AdversarialSeal@users.noreply.github.com>
2023-11-27 11:14:34 -0500

    Adicion de src

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,40 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'actividad-analyzer';
+export class AppComponent  {
+
+  myForm!: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit() {
+    this.myForm = this.formBuilder.group({
+      des: ['', Validators.required],
+      dem: ['', Validators.required],
+      pasos: ['', Validators.required],
+      vmemps: ['', Validators.required],
+      rpempk: ['', Validators.required],
+      datnm: ['', Validators.required],
+      fcmelpm: ['', Validators.required],
+    });
+  }
+
+  onSubmit() {
+    // Lógica para manejar el envío del formulario
+    const formData = this.myForm.value;
+
+    if(Object.values(formData).some(value => value === '')){
+    
+      console.log('Existen datos "Maliciosos:', this.myForm.value);
+    }else{
+
+      console.log('Formulario enviado:', this.myForm.value);
+
+    }
+  }
 }
